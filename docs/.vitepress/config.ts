a8464aceb8327107c3fd99bd0a0d471445ee6a38
95cc <71340227+95cc@users.noreply.github.com>
2023-05-04 22:14:05 +0800

    更新base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: 'site',
+  base: '95cc-site',
   title: "95cc",
   titleTemplate: ':title',
   description: "Recoding something.",
